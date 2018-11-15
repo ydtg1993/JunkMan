bae20e2c850a041a81ec984c3ea03966a2e964e9
946818508@qq.com <scl199303>
2018-11-15 14:07:53 +0800

    optimize

--- src/Application.php
+++ src/Application.php
@@ -14,10 +14,12 @@ class Application
     public function run()
     {
         self::config();
-        Ems::examine();
+        self::setExamine();
+        self::setXdebug();
         self::secret();
         self::setTemp(Defined::getSECRET());
         self::setSocketHead();
+        self::setTraceFile();
     }
 
     private static function config()
@@ -55,5 +57,51 @@ class Application
             'secret' => Defined::getSECRET()
         ];
         Defined::setSOCKETHEAD($data);
+    }
+
+    private static function setExamine()
+    {
+        date_default_timezone_set('Asia/Shanghai');
+        Defined::setTIME(time());
+    }
+
+    private static function setXdebug()
+    {
+        if (!function_exists('debug_backtrace')) {
+            throw new \Exception('Need to install Xdebug');
+        }
+        ini_set('xdebug.collect_params', 4);
+        ini_set('xdebug.collect_return', 1);
+        ini_set('xdebug.show_mem_delta', 0);
+        ini_set('xdebug.collect_assignments', 1);
+        ini_set('xdebug.collect_includes', 0);
+        ini_set('xdebug.trace_format', 0);
+        ini_set('xdebug.profiler_enable', 1);
+        ini_set('xdebug.var_display_max_depth', 10);
+        ini_set('collect_assignments', 1);
+        ini_set('xdebug.coverage_enable', 1);
+    }
+
+    private static function setTraceFile()
+    {
+        $call_func_data = self::multiQuery2Array(debug_backtrace(), ['function' => 'start', 'class' => 'Stream']);
+        Defined::setTraceFile($call_func_data['file']);
+    }
+
+    private static function multiQuery2Array($array, array $params)
+    {
+        foreach ($array as $item) {
+            $add = true;
+            foreach ($params as $field => $value) {
+                if ($item[$field] != $value) {
+                    $add = false;
+                }
+            }
+            if ($add) {
+                return $item;
+            }
+        }
+
+        return [];
     }
 }
