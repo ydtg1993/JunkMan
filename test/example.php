1b56cb96d2d4eebaf802181f1e3ee66858e71d1e
946818508@qq.com <scl199303>
2018-11-28 10:41:43 +0800

    *

--- test/example.php
+++ test/example.php
@@ -6,12 +6,13 @@
  * Time: 下午 5:55
  */
 require_once __DIR__.'/../autoload.php';
-
+use JunkMan\JunkMan;
 //测试调用
 //test1();
 //test2();
 //test3();
 //test4();
+
 
 /**
  * stream
@@ -19,14 +20,14 @@ require_once __DIR__.'/../autoload.php';
  */
 function test1()
 {
-    \JunkMan\JunkMan::stream()->start('stream test');
+    JunkMan::stream()->start('stream test');
     //测试代码
     $array = [1, 2, 3, 4];
     $total = 0;
     foreach ($array as $a) {
         $total = $total + $a;
     }
-    \JunkMan\JunkMan::stream()->end();
+    JunkMan::stream()->end();
 }
 
 /**
@@ -35,16 +36,16 @@ function test1()
  */
 function test2()
 {
-    \JunkMan\JunkMan::flush()->start('flush test');
+    JunkMan::flush()->start('flush test');
     //测试代码
     $i = 1;
     $data = [];
     while($i <= 10){
         $data[] = $i;
-        \JunkMan\JunkMan::flush()->refurbish();
+        JunkMan::flush()->refurbish();
         sleep(1);
     }
-    \JunkMan\JunkMan::flush()->end();
+    JunkMan::flush()->end();
 }
 
 /**
@@ -55,7 +56,7 @@ function test3()
 {
     //测试代码
     $data = [1,2,3,4,5];
-    \JunkMan\JunkMan::spot()->dot('dot test',$data);
+    JunkMan::spot()->dot('dot test',$data);
 }
 
 /**
@@ -64,8 +65,8 @@ function test3()
  */
 function test4()
 {
-    \JunkMan\JunkMan::stream()->start('stream test');
+    JunkMan::stream()->start('stream test');
     //测试代码
     $a = 1 / 0;
-    \JunkMan\JunkMan::stream()->end();
+    JunkMan::stream()->end();
 }
